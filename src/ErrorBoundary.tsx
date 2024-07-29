912eecb02a425ec0a730496bfb7467fd80aa7df0
saisharath04 <sharath04122000@gmail.com>
2024-07-29 10:12:35 +0530

    error boundry is added

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(
+      "%c errorBoundaryError! ",
+      "background: #222; color: red",
+      error,
+      errorInfo
+    );
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return <h1>Sorry.. there was an error</h1>;
+    }
+
+    return <>{children}</>;
+  }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/store.ts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
+import ErrorBoundary from "./ErrorBoundary.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </ErrorBoundary>
 );
